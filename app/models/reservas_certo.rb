d70b87ddbbabcf8c64add8116624c6b6d49a7050
Adriano Antongiovanni <adrianoaugusto98@mail.com>
2020-12-13 17:07:13 -0300

    Reserva Ajeitada

--- app/models/reservas_certo.rb
+++ app/models/reservas_certo.rb
@@ -1,2 +1,19 @@
 class ReservasCerto < ApplicationRecord
-end
+    belongs_to :formulario_certo
+    belongs_to :user
+
+    def create
+      formulario = Formulario_certo.find(params[:formulario_certo.id])
+      @reservas_certo = @formulario_certo.items.build(formulario: formulario_certo)
+
+      respond_to do |format|
+        if @reservas_certo 
+        format.html { redirect_to @line_item.formulario_certo, notice: 'Criado'}
+        format.json {render :show, status: :created, location: @reservas_certo}
+        else 
+        format.html{render :new}
+        format.json {render json: @line_item.errors, status: :unprocessable_entity}
+        end
+    end
+end 
+ end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  has_many :reservas_certo
+  before_destroy :ensure_not_referenced_by_any_reservas_certo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+  private
+    def ensure_not_referenced_by_any_reservas_certo
+      unless reservas_certo.empty?
+          error.add(:base, 'Reservas presente')
+          thow :abort
+        end
+      end
+    end
